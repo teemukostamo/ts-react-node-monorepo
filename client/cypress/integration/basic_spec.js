b9c59f2c0b739e797aa5a30efee325e0044e0e45
Teemu Kostamo <teemu.kostamo@hotmail.com>
2020-11-21 20:04:12 +0200

    removed boilerplate

--- client/cypress/integration/basic_spec.js
+++ client/cypress/integration/basic_spec.js
@@ -1,10 +1,11 @@
-describe('client ', function () {
-  beforeEach(function () {
+/* eslint-disable no-undef */
+describe('client ', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:3000');
   });
 
-  it('front page can be opened', function () {
+  it('front page can be opened', () => {
     cy.visit('http://localhost:3000');
-    cy.contains('Edit src/App.tsx and save to reload.');
+    cy.contains('Happy coding!');
   });
 });
